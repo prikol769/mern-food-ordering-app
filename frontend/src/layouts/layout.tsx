f1f3866e745cbb3f72fec5f35b041405abd31fc6
prikol769 <prikol769@gmail.com>
2024-04-09 15:25:38 -0500

    added Layout component

---
+++ frontend/src/layouts/layout.tsx
@@ -0,0 +1,16 @@
+import Header from "@/components/Header";
+
+type LayoutProps = {
+  children: React.ReactNode;
+};
+
+const Layout = ({ children }: LayoutProps) => {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <div className="container mx-auto flex-1 py-10">{children}</div>
+    </div>
+  );
+};
+
+export default Layout;
