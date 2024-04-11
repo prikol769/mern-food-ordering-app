d7bf61643a7ba761786d8677f8aff9f61f73c5ba
prikol769 <prikol769@gmail.com>
2024-04-10 21:55:57 -0500

    added showHero prop

--- frontend/src/layouts/layout.tsx
+++ frontend/src/layouts/layout.tsx
@@ -4,13 +4,14 @@ import Hero from "@/components/Hero";
 
 type LayoutProps = {
   children: React.ReactNode;
+  showHero?: boolean;
 };
 
-const Layout = ({ children }: LayoutProps) => {
+const Layout = ({ children, showHero = false }: LayoutProps) => {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
-      <Hero />
+      {showHero && <Hero />}
       <div className="container mx-auto flex-1 py-10">{children}</div>
       <Footer />
     </div>
